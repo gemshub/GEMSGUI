15f6a8721af926d7910aa29bb269f56789a081d3
gems <gems@7d9feeff-4400-0410-95ed-9cb8b92a518d>
2003-11-27 16:06:03 +0000

    Upgrade to Qt 3.2.3
    git-svn-id: svn+ssh://savannah01.psi.ch/repos/gems/GEMSELEKTOR/trunk@235 7d9feeff-4400-0410-95ed-9cb8b92a518d

--- gems2/vizor/visor_w.cpp
+++ gems2/vizor/visor_w.cpp
@@ -63,13 +63,13 @@ using namespace std;
 const char* GEMS_LOGO_ICON = "img/gems1.png";
 const char* GEMS_SYS_ICON = "img/sciences_section.xpm";
 const char* GEMS_VERSION_STAMP = "GEM-Selektor v2.0.0-PSI (Linux)";
-const char* GEMS_DEFAULT_FONT_NAME = "Lucidatypewriter";
-const int GEMS_DEFAULT_FONT_SIZE = 12;
+const char* GEMS_DEFAULT_FONT_NAME = "Courier New";
+const int GEMS_DEFAULT_FONT_SIZE = 10;
 #else
 const char* GEMS_LOGO_ICON = "img/gems1.png";
 const char* GEMS_SYS_ICON = "img/sciences_section.xpm";
 const char* GEMS_VERSION_STAMP = "GEM-Selektor v2.0.0-PSI (Win32)";
-const char* GEMS_DEFAULT_FONT_NAME = "Courier";
+const char* GEMS_DEFAULT_FONT_NAME = "Courier New";
 const int GEMS_DEFAULT_FONT_SIZE = 9;
 #endif
 extern const char* GEMS_ABOUT_HTML;
