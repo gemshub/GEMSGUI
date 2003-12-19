f47344995541de2305d126b21960f0c5a3cafd83
gems <gems@7d9feeff-4400-0410-95ed-9cb8b92a518d>
2003-12-19 09:37:06 +0000

    Added FMT-Bridge directory and zero version of FMT-GEM Data Bridge headers
    git-svn-id: svn+ssh://savannah01.psi.ch/repos/gems/GEMSELEKTOR/trunk@236 7d9feeff-4400-0410-95ed-9cb8b92a518d

---
+++ gems2/mods/gemipm/FMT-Bridge/datach.h
@@ -0,0 +1,169 @@
+//-------------------------------------------------------------------
+Data Bridge Chemistry structure  (draft variant 2)
+//-------------------------------------------------------------------
+//
+#ifndef _DataCh_H_
+#define _DataCh_H_
+
+typedef struct
+{  // Structure DataCH
+   // Dimensionalities
+  unsigned short 
+   // these dimensionalities should be the same as in MULTI (GEM IPM)
+    nIC,        // total number of stoichiometry units in reactive part 
+    nDC,      	// total number of DC (chemical species) in reactive part
+    nPH,       	// total number of phases included into the IPM problem
+    nPS,        // number of multicomponent phases <= nPH
+    nTp,        // Number of temperature points in TD arrays
+    nPp,        // Number of pressure points in TD arrays
+    uRes1,
+    uRes2,
+
+  // These dimensionalities define sizes of dynamic data in DATABT structure!!!
+  // Needed to reduce on storage demand for data bridge instances (nodes)!
+  // Connection occurs through xIC, xPH and xDC lists!
+    nICb,       // number of stoichiometry units (<= nIC) used in the data bridge  
+    nDCb,      	// number of DC (chemical species, <= nDC) used in the data bridge
+    nPHb,     	// number of phases (<= nPH) used in the data bridge
+    nPSb,       // number of multicomponent phases (<= nPS) used in the data bridge
+    uRes3,
+    uRes4,
+
+// lists, vectors and matrices
+    *nDCinPH,  // number of DC included into each phase [nPH]
+
+// These lists of indices connect the DATABR arrays with this structure
+    *xIC,   // ICNL indices in DATABR IC vectors [nICb]
+    *xDC,   // DCNL indices in DATABR DC list [nDCb]
+    *xPH;   // PHNL indices in DATABR phase vectors [nPHb]
+
+  float 
+    *A;     // DC stoichiometry matrix A composed of a_ij [nIC][nDC]
+            // float reduces storage demand here 
+  double 
+    Tmin, Tmax, Tstep, Ttol, // Temperature T, K, min.,-max., step, tolerance
+    Pmin, Pmax, Pstep, Ptol, // Pressure P, bar, min.,-max., step, tolerance
+    dRes1, dRes,
+
+// Data vectors
+// IC - related values
+    *ICmm,   // IC atomic (molar) mass, g/mole [nIC]
+
+// DC - related values  
+    *DCmm,   // DC molar mass, g/mole [nDC]
+// Thermodynamic data
+// Require Lagrange extrapolation subroutine to extract data for given P,T    
+    *G0,    // standard molar Gibbs energy [nDC, nPp, nTp]
+    *V0,    // standard molar volume [nDC, nPp, nTp]
+    *H0,    // standard molar enthalpy, reserved [nDC, nPp, nTp]
+    *Cp0,   // st. molar heat capacity, reserved [nDC, nPp, nTp]
+
+// Phase-related data 
+    *Aalp;  // specific surface areas of phases in m2/g [nPH] 
+
+// Name lists
+ char
+  (*ICNL)[MaxICN], // List of IC names in the system [nIC]
+  (*DCNL)[MaxDCN], // List of DC names in the system [nDC]
+  (*PHNL)[MaxPHN], // List of phase names in the system [nPH]
+
+// Class code lists
+    *ccIC,   // Class codes of IC [nIC], see  ICL_CLASSES
+    *ccDC,   // Class codes of DC [nDC]  see  DCL_CLASSES 
+    *ccDCW,  // Generic codes for DC [nDC], see SolDCLcodes  ??? 
+    *ccPH;   // Class codes of phases [nPH], see PHL_CLASSES 
+}
+DATACH;
+
+const unsigned int
+    MaxICN =      6,
+    MaxDCN =      16,
+    MaxPHN =      16;
+
+/* Work DC classifier codes  ccDCW ??? */
+enum SolDCLcodes {
+    DCl_SINGLE = 'U',        /* This DC is a single-component phase */
+    DCl_SYMMETRIC = 'I',     /* This DC is in symmetric solution phase */
+    DCl_ASYM_SPECIES = 'S', /*This is DC-solute(sorbate) in asymmetric phase */
+    DCl_ASYM_CARRIER = 'W'  /*This is carrier(solvent) DC in asymmetric phase*/
+};
+
+
+typedef enum {  /* classes of IC*/
+    IC_ELEMENT  =  'e',  // chemical element (except oxygen and hydrogen)
+    IC_OXYGEN   =  'o',  // oxygen
+    IC_HYDROGEN =  'h',  // hydrogen (natural mixture of isotopes) H
+    IC_PROTIUM   = 'p',  // protium (reserved) Hp
+    IC_DEYTERIUM = 'd',  // deuterium (reserved) D
+    IC_TRITIUM  =  't',  // tritium (reserved) T
+    IC_FORMULA  =  'f',  // formula unit (eg. for Sio - a symbol of SiO2)
+    IC_METALION =  'm',  // metal ion (cation), reserved
+    IC_LIGAND   =  'l',  // ligand (anion), reserved
+    IC_ADDIT    =  'a',  // IC with unknown stoichiometry (eg: Hum, humic acid)
+    IC_ISOTOPE  =  'i',  // isotope of chemical element from 1 to 250
+    IC_OXYGEN16 =  'q',  // q  - oxygen 16O (reserved)
+    IC_OXYGEN18 =  'r',  //  r  - oxygen 18O (reserved)
+    IC_CHARGE   =  'z',  // z  - electrical charge
+    IC_VOLUME   =  'v'   // volume
+} ICL_CLASSES;
+
+typedef enum {  /* Classifications of DC */
+    /*Aqueous electrolyte phase:*/
+    DC_AQ_PROTON   = 'T',      // hydrogen ion H+
+    DC_AQ_ELECTRON = 'E',      // electron (as a DC)
+    DC_AQ_SPECIES  = 'S',      // other aqueous species (ions, complexes and ion pairs)
+    DC_AQ_SOLVENT  = 'W',      // water H2O (major solvent)
+    DC_AQ_SOLVCOM  = 'L',      // other components of a solvent (eg. alcohol)
+    /*Gas phase ( G code can be used for all gases; V,C,H,N codes are reserved
+    for future use of the built-in equations of state in FGL module): */
+    DC_GAS_COMP    = 'G',   // other gases
+    DC_GAS_H2O     = 'V',   // H2O steam
+    DC_GAS_CO2     = 'C',   // CO2 (carbon dioxide)
+    DC_GAS_H2      = 'H',   // H2 hydrogen
+    DC_GAS_N2      = 'N',   // N2 nitrogen
+    /* Solid/liquid non-electrolyte multicomponent phases:*/
+    DC_SOL_IDEAL   = 'I',   // end-member component with ideal behaviour
+    DC_SOL_MINOR   = 'J',   // junior component (Henry's Law)
+    DC_SOL_MAJOR   = 'M',   // major component (Raoult's Law)
+    /* Sorption phases and poly(oligo)electrolytes */
+    DC_SUR_CARRIER = 'Q',   // Principal end-member of solid carrier
+    DC_SUR_MINAL   = 'P',   // Minor end-member of solid carrier
+    DC_PEL_CARRIER = 'R',   // Carrier of poly(oligo)electrolyte for future use
+
+    DC_SSC_A0 = '0', DC_SSC_A1 = '2', DC_SSC_A2 = '4', DC_SSC_A3 = '6',
+    DC_SSC_A4 = '8', /* Strong surface complex on site type 0,1,2,3,4 - A plane */
+    DC_WSC_A0 = '1', DC_WSC_A1 = '3', DC_WSC_A2 = '5', DC_WSC_A3 = '7',
+    DC_WSC_A4 = '9', /* Weak surface complex on site type 0,1,2,3,4 - B plane */
+    DC_IESC_A  = 'A', /* Strong exchange ion const-charge plane */
+    DC_IEWC_B  = 'B', /* Weak exchange ion const-charge plane */
+
+    /* Aliaces for 1-site model */
+    DC_SUR_SITE    = 'X',  /* Surface site A plane -> '0' */
+    DC_SUR_COMPLEX = 'Y',  /* Strong sur. complex A plane -> '0' */
+    DC_SUR_IPAIR   = 'Z',  /* Weak sur complex B plane -> '1' */
+
+    /* Single-component phases:*/
+    DC_SCP_CONDEN  = 'O',   // DC forming a single-component phase
+
+    /* Here add some special classes for diffusion etc. */
+    DCaquoCATION   = 'c', 
+    DCaquoANION    = 'n', 
+    DCaquoLIGAND   = 'l', 
+    DCaquoCOMPLEX  = 'x',
+    DCaquoIONPAIR  = 'p',
+    DCaquoGAS      = 'g',
+} DCL_CLASSES;
+
+typedef enum {  /* Possible values */
+    PH_AQUEL    = 'a',  // aqueous electrolyte
+    PH_GASMIX   = 'g',  // mixture of gases
+    PH_FLUID    = 'f',  // fluid phase
+    PH_SOLUTION = 'l',  // non-electrolyte liquid (melt)
+    PH_SORPTION = 'x',  // dilspersed solid with adsorption (ion exchange) in aqueous
+    PH_POLYEL = 'y',    // colloidal poly- (oligo)electrolyte
+    PH_SINCOND  = 's',  // condenced solid phase, also multicomponent
+    PH_SINDIS   = 'd',  // dispersed solid phase, also multicomponent
+} PHL_CLASSES;
+
+#endif   //_DataCH_H
+
---
+++ gems2/mods/gemipm/FMT-Bridge/m_const.h
@@ -0,0 +1,231 @@
+
+const int MST =  6;
+const unsigned int
+    MAXICNAME =      6,
+    MAXSYMB =        4,
+    TDBVERSION =     64;
+
+
+const int
+    MAXDCNAME =      16,
+    MAXPHNAME =      16,
+    EQ_RKLEN = 58;
+
+#include <ctype.h>
+#include <fstream>
+using namespace std;
+
+// dynamically allocated temporary 'char*'
+// for simple string manipulations
+// (used instead of stack char[] allocation to avoid stack problems)
+struct vstr
+{
+    char* p;
+    vstr(int ln): p(new char[ln+1])
+    { }
+
+    vstr(int ln, const char* s): p(new char[ln+1])
+    {
+        strncpy(p, s, ln);
+        p[ln]='\0';
+    }
+
+    vstr(const char* s): p(new char[strlen(s)+1])
+    {
+       strcpy(p, s);
+    }
+
+    ~vstr()
+    {
+        delete[] p;
+    }
+
+    operator char* ()
+    {
+        return p;
+    }
+
+private:    vstr (const vstr&);
+    const vstr& operator= (const vstr&);
+
+};enum solmod_switches { /* indexes of keys of model solution*/
+    SPHAS_TYP, DCOMP_DEP, SPHAS_DEP, SGM_MODE, DCE_LINK, SCM_TYPE,
+    /* link state */
+    LINK_UX_MODE, LINK_TP_MODE, LINK_FIA_MODE,
+    /* Posible values of аof keys of model solution - DCOMP_DEP, SPHAS_DEP */
+    SM_UNDEF = 'N', SM_TPDEP = 'T', SM_UXDEP = 'X', SM_PRIVATE = 'P',
+    SM_PUBLIC = 'U',
+    // Posible modes calculating of activity coefficients SGM_MODE
+    SM_STNGAM = 'S', SM_NOSTGAM = 'N',
+    /* This code (one upper-case letter or digit) defines type of mixing
+    and default method of calculation of mixing properties before and at
+    IPM iterations.   Possible values: (SPHAS_TYP) */
+    SM_IDEAL =  'I', // ideal solution or single-component phase;
+    SM_REDKIS = 'G', // built-in Guggenheim (Redlich-Kister) binary SS model
+                     // (with 3 coeffs)
+    SM_MARGB = 'M',  // built-in binary Margules SS (subreg. w. 3 coeff U,P,T )
+    SM_MARGT = 'T',  // built-in ternary Margules SS (reg. w. 3 coeff U,P,T )
+    SM_FLUID = 'F',  // built-in multicomp. EOS fluid Churakov&Gottschalk 2003
+    SM_RECIP = 'R',  // reciprocal solution (to be done), formerly regular solution
+    SM_AQDAV = 'D',  // built-in Davies equation (with 0.3) added KD 25.01.02
+    SM_AQDH1 = '1',  // built-in limiting Debye-Hueckel law for aqueous species
+    SM_AQDH2 = '2',  // built-in 2-term Debye-Hueckel (Kielland)
+    SM_AQDH3 = '3',  // built-in 3-d approximation of Debye-Hueckel
+    SM_AQDHH = 'H',  // built-in 3-d approximation of Debye-Hueckel (Helgeson)
+    SM_AQSIT = 'S',  // built-in SIT model for aq activity coeffs (reserved)
+    SM_AQPITZ = 'P', // Pitzer's model for aqueous brines (reserved)
+    SM_IONEX = 'E',  // ion exchange (Donnan, Nikolskii) (reserved)
+    SM_SURCOM = 'A', // models of surface complexation at solid-aqueous interface
+    SM_USERDEF = 'U', // user-defined mixing model (in Phase record)
+    SM_OTHER = 'O'   //  other models of non-ideal solutions (reserved)
+};
+
+typedef enum {  /* Classifications of DC */
+    /* Type of input data for */
+    SRC_DCOMP =  'd',  // the key points to existing PDB record in DCOMP chain
+    SRC_REACDC = 'r',  // the key points to existing PDB record in REACDC chain
+    SRC_NEWDC =  'n',  // the key new reaction-defined component
+    SRC_NEWISO = 'i',  // the same as n, but this component is an isotopic form
+    SRC_FICT =   'f',  // fictive species
+    /*Aqueous electrolyte phase:*/
+    DC_AQ_PROTON   = 'T',      // hydrogen ion H+
+    DC_AQ_ELECTRON = 'E',      // electron (as a DC)
+    DC_AQ_SPECIES  = 'S',      // other aqueous species (ions, complexes and ion pairs)
+    DC_AQ_SOLVENT  = 'W',      // water H2O (major solvent)
+    DC_AQ_SOLVCOM  = 'L',      // other components of a solvent (eg. alcohol)
+    /*Gas phase ( G code can be used for all gases; V,C,H,N codes are reserved
+    for future use of the built-in equations of state in FGL module): */
+    DC_GAS_COMP    = 'G',   // other gases
+    DC_GAS_H2O     = 'V',   // H2O steam
+    DC_GAS_CO2     = 'C',   // CO2 (carbon dioxide)
+    DC_GAS_H2      = 'H',   // H2 hydrogen
+    DC_GAS_N2      = 'N',   // N2 nitrogen
+    /* Solid/liquid non-electrolyte multicomponent phases:*/
+    DC_SOL_IDEAL   = 'I',   // end-member component with ideal behaviour
+    DC_SOL_MINOR   = 'J',   // junior component (Henry's Law)
+    DC_SOL_MAJOR   = 'M',   // major component (Raoult's Law)
+    /* Sorption phases and poly(oligo)electrolytes */
+    DC_SUR_CARRIER = 'Q',   /* Principal end-member of solid carrier */
+    DC_SUR_MINAL   = 'P',   /* Minor end-member of solid carrier */
+    DC_PEL_CARRIER = 'R',   /* Carrier of poly(oligo)electrolyte */
+
+    DC_SSC_A0 = '0', DC_SSC_A1 = '2', DC_SSC_A2 = '4', DC_SSC_A3 = '6',
+    DC_SSC_A4 = '8', /* Strong surface complex on site type 0,1,2,3,4 - A plane */
+    DC_WSC_A0 = '1', DC_WSC_A1 = '3', DC_WSC_A2 = '5', DC_WSC_A3 = '7',
+    DC_WSC_A4 = '9', /* Weak surface complex on site type 0,1,2,3,4 - B plane */
+    DC_IESC_A  = 'A', /* Strong exchange ion const-charge plane */
+    DC_IEWC_B  = 'B', /* Weak exchange ion const-charge plane */
+
+    /* Aliaces for 1-site model */
+    DC_SUR_SITE    = 'X',  /* Surface site A plane -> '0' */
+    DC_SUR_COMPLEX = 'Y',  /* Strong sur. complex A plane -> '0' */
+    DC_SUR_IPAIR   = 'Z',  /* Weak sur complex B plane -> '1' */
+
+    /* Single-component phases:*/
+    DC_SCP_CONDEN  = 'O',   // DC forming a single-component phase
+
+    /* Дополнительные коды для печати результатов расчета равновесий */
+    DC_AQA_CATION   = 'c',  /* Катион */
+    DC_AQA_ANION    = 'n',  /* Анион */
+    DC_AQA_LIGAND   = 'l',  /* Органический лиганд */
+    DC_AQA_COMPLEX  = 'x',  /* Комплекс в водном растворе */
+    DC_AQA_IONPAIR  = 'p',  /* Ионная пара */
+    DC_AQA_GAS      = 'g',  /* Растворенный газ (нейтральн.) */
+    DC_AQA_ACID     = 'a',  /* Кислота */
+    DC_AQA_BASE     = 'b',  /* Щелочь */
+    DC_AQA_SALT     = 's',  /* Соль (недиссоц. электролит) */
+    DC_AQA_HYDROX   = 'o'   /* Ион гидроксила */
+
+} DC_CLASSES;
+
+//    This code defines standard state and reference scale of concentra-
+// tions for components of this phase. It is used by many subroutines
+// during calculations of equilibrium states
+enum PH_CLASSES{  /* Possible values */
+    PH_AQUEL    = 'a',  // aqueous electrolyte
+    PH_GASMIX   = 'g',  // mixture of gases
+    PH_FLUID    = 'f',  // fluid phase
+    PH_PLASMA   = 'p',  // plasma
+    PH_SOLUTION = 'l',  // non-electrolyte liquid (melt)
+    PH_SIMELT   = 'm',  // silicate (magmatic) melt or non-aqueous electrolyte
+    PH_SORPTION = 'x',  // dilspersed solid with adsorption (ion exchange) in aqueous
+    PH_POLYEL = 'y',    // colloidal poly- (oligo)electrolyte
+    PH_SINCOND  = 's',  // condenced solid phase, also multicomponent
+    PH_SINDIS   = 'd',  // dispersed solid phase, also multicomponent
+    PH_HCARBL   = 'h'   // mixture of condensed hydrocarbons
+};
+
+typedef enum {  /* Limits on DC and phases */
+    /* type of lmits */
+    NO_LIM = 'O', LOWER_LIM ='L', UPPER_LIM = 'U', BOTH_LIM ='B',
+    /* mode recalc of limits Set_DC_Limits() */
+    DC_LIM_INIT = 0, DC_LIM_CURRENT = 1
+} DC_LIMITS;
+
+enum sorption_control {
+    /* EDL interface models - separate for site types in v. 3.1 */
+    SC_DDLM = 'D',  SC_CCM = 'C',     SC_TLM = 'T',   SC_MTL = 'M',
+    SC_MXC = 'E',   SC_NNE = 'X',     SC_IEV  = 'I',  SC_BSM = 'S',
+    SC_NOT_USED = 'N',
+    /* Methods of Surface Activity Terms calculation */
+    SAT_COMP = 'C', SAT_NCOMP = 'N', SAT_SITE = 'S', SAT_INDEF = 'I',
+    /* Assignment of surtype to carrier (end-member) */
+    CCA_VOL = 'V', CCA_0 = '0', CCA_1, CCA_2, CCA_3, CCA_4, CCA_5,
+    CCA_6, CCA_7, CCA_8, CCA_9
+};
+
+typedef enum { // Units of measurement of quantities and concentrations
+    /* number of components and phases */
+    QUAN_MKMOL = 'Y',  QUAN_MMOL = 'h',  QUAN_MOL = 'M', // NUMBER OF MOLES
+    QUAN_MGRAM = 'y',  QUAN_GRAM = 'g',  QUAN_KILO = 'G',// MASS
+    /* concentrations of components and phases*/
+    CON_MOLFR = 'n', CON_MOLPROC = 'N', CON_pMOLFR = 'f', // MOLE FRACTION
+    CON_VOLFR = 'v', CON_VOLPROC = 'V', CON_pVOLFR = 'u', // VOLUME FRACTION
+    CON_MOLAL = 'm', CON_MMOLAL =  'i', CON_pMOLAL = 'p', // MOLALITY
+    CON_MOLAR = 'L', CON_MMOLAR =  'j', CON_pMOLAR = 'q', // MOLARITY
+    CON_WTFR  = 'w', CON_WTPROC =  '%', CON_PPM =    'P', // MASS FRACTION
+    CON_AQWFR = 'C', CON_AQWPROC = 'c', CON_AQPPM =  'a', // CONCENTRATION
+    // aqueous species
+    CON_AQGPL = 'd', CON_AQMGPL = 'e', CON_AQMKGPL = 'b',//VOLUME CONCENTRATION
+
+    /* поверхностных величин */
+    SUR_AREAM = 's',  SUR_AREAN = 'S', /* Уд.поверхность м2/г, м2/мол */
+    SUR_CONA  = 'A', /* Поверхн. избыток мол/м2 */
+    SUR_CONM  = 'D', /* Поверхн. избыток мол/г */
+    SUR_CONN  = 'E', /* Поверхн. избыток мол/мол */
+
+    //Units of measurement of pressure Pr, P  { b B p P A }'
+    PVT_BAR =  'b', // bar - default, 1 bar = 1e5 Pa
+    PVT_KBAR = 'B', // kbar, 1 kbar = 1000 bar
+    PVT_PASC = 'p', // Pascal (Pa)
+    PVT_KPASC = 'P',// MPa, 1 MPa = 10 bar = 1e6 Pa
+    PVT_ATM =  'A', // atm, 1 atm = 1.013 bar
+    //Attention: Only b code can be used in this version!
+
+    //Units of measurement of molar volume  { c j a L m }'
+    PVT_CM3 =  'c', /*cm3, cm3/mole*/
+    PVT_LITR =  'L', // liters (L) - volume of the system only, 1 L = 1000 cm3
+    PVT_JBAR =  'j', // J/bar, 10 cm3/mole = 1 J/bar
+    PVT_CBAR = 'a',  // (cal/bar), 41.84 cm3/mole = 1 cal/bar
+    // m  - reserved.
+    //Attention: only j code can be used in this version!
+
+    //Units of measurement of reference temperature Tr { C K F }'
+    PVT_CELS = 'C', /*degrees Celsius (C)*/
+    PVT_KELVIN = 'K', /*Kelvins (K), 0 C = 273.15 K*/
+    PVT_FAREN = 'F',  /*degrees Fahrenheit (F)*/
+    //Attention: Only C code can be used in this version.
+
+    // Units of measurement of energy values { j c J C n N }
+    TDAT_JOUL = 'j',/* Joules (J/mole)*/ TDAT_KJOUL = 'J', /*kilojoules (kJ/mole)*/
+    TDAT_CAL = 'c', /* calories (cal/mole); 1 cal = 4.184 J; */
+    TDAT_KCAL = 'C', /*kilocalories (kcal/mole)*/
+    TDAT_NORM = 'N' /*normalized (mole/mole, J/mole/RT, cal/mole/RT)*/
+                // Attention: Only j code can be used in this version!
+} SPPA_UNITS;
+
+const char S_OFF = '-',
+                   S_ON = '+',
+                          S_REM = '*',
+                                  A_NUL ='?';
+
