d9d0ff34b653232d5ce846a2435f72e6d0cb2457
andy <andy@7d9feeff-4400-0410-95ed-9cb8b92a518d>
2004-07-06 09:24:39 +0000

    - Apple compile fixes
    git-svn-id: svn+ssh://savannah01.psi.ch/repos/gems/GEMSELEKTOR/trunk@277 7d9feeff-4400-0410-95ed-9cb8b92a518d

--- gems2/rungems.sh
+++ gems2/rungems.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+if [ "`uname`" == "Darwin" ]; then 
+exec `pwd`/gems2.app/Contents/MacOS/gems2 -s ~/Gems2Lin/program -u ~/Gems2Lin 
+else
 exec ./gems2 -s ~/Gems2Lin/program -u ~/Gems2Lin
+fi
+
