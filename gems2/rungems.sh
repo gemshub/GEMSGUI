c3c5acf7071ce3e60b9d76f669ce60574a0fbd51
andy <andy@7d9feeff-4400-0410-95ed-9cb8b92a518d>
2004-07-12 11:19:44 +0000

    - distribution fixes
    git-svn-id: svn+ssh://savannah01.psi.ch/repos/gems/GEMSELEKTOR/trunk@303 7d9feeff-4400-0410-95ed-9cb8b92a518d

--- gems2/rungems.sh
+++ gems2/rungems.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ "`uname`" == "Darwin" ]; then 
-exec `pwd`/gems2.app/Contents/MacOS/gems2 -s ~/Gems2Data/program -u ~/Gems2Data
+exec `pwd`/gems2.app/Contents/MacOS/gems2 $@
 else
 #exec ./gems2 -c -s ~/Gems2Data/program -u ~/Gems2Data
-exec ./gems2 -c
+exec ./gems2 -c $@
 fi
 
