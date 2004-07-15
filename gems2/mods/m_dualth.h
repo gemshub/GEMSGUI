5e1432a10866be10f7a78d74c9ef5d7ef70249cc
gems <gems@7d9feeff-4400-0410-95ed-9cb8b92a518d>
2004-07-15 16:03:11 +0000

    Extensions in DualThermo (DK)
    git-svn-id: svn+ssh://savannah01.psi.ch/repos/gems/GEMSELEKTOR/trunk@336 7d9feeff-4400-0410-95ed-9cb8b92a518d

--- gems2/mods/m_dualth.h
+++ gems2/mods/m_dualth.h
@@ -32,131 +32,133 @@ const   int MAXFORMUNITDT=     40;
 typedef struct
 { // Description of DualTh data structure (revised in July 2004)
   // Record key format: the same as Proces
-    char
-PunE,          // Units of energy   { j;  J c C N reserved }
-PunV,          // Units of volume   { j;  c L a reserved }
-PunP,          //  Units of pressure  { b;  B p P A reserved }
-PunT,          // Units of temperature  { C; K F reserved }
+  char
+   PunE,          // Units of energy   { j;  J c C N reserved }
+   PunV,          // Units of volume   { j;  c L a reserved }
+   PunP,          //  Units of pressure  { b;  B p P A reserved }
+   PunT,          // Units of temperature  { C; K F reserved }
 
 // Allocation flags
-PvCoul,   // Use coulombic factors for non-basis DCs (+ -)
-PvICb,    // Use IC quantities for basis sub-system compositions? { + * - }
-PvICn,    // Use IC quantities for non-basis sub-system compositions? { + * - }
-PvAUb,    // Use formula units for basis sub-system compositions? { + * - }
-PvSd,     // Include references to data sources (+ -)?
-PvChi,    // Use math script for mole fractions of non-basis DCs (+ -)?
-PvGam,    // Use math script for activity coeffs of non-basis DCs (+ -)?
-PvRes,  // Reserved
+   PvCoul,   // Use coulombic factors for non-basis DCs (+ -)
+   PvICb,    // Use IC quantities for basis sub-system compositions? { + * - }
+   PvICn,    // Use IC quantities for non-basis sub-system compositions? { + * - }
+   PvAUb,    // Use formula units for basis sub-system compositions? { + * - }
+   PvSd,     // Include references to data sources (+ -)?
+   PvChi,    // Use math script for mole fractions of non-basis DCs (+ -)?
+   PvGam,    // Use math script for activity coeffs of non-basis DCs (+ -)?
+PvTP,     // Use vectors for T and P of experiments (+ -)?
 
      // Controls on operation
-PsMode,  // DualTh mode of operation { M G A X }
-PsSt,    // State of non-basis part saturation { E P S }
+   PsMode,  // DualTh mode of operation { M G A X }
+   PsSt,    // State of non-basis part saturation { E P S }
 
 //  Status and control flags (+-)
-gStat,  // DualTh generation status: 0 -indefinite; 1 on-going generation run;
+   gStat,  // DualTh generation status: 0 -indefinite; 1 on-going generation run;
         //  2 - done generation run; 3 - generation run error (+ 5: the same in stepwise mode)
-aStat,  // DualTh analysis status:  0 - indefinite; 1 ready for analysis;
+   aStat,  // DualTh analysis status:  0 - indefinite; 1 ready for analysis;
         //  2 - analysis run; 3 - analysis done; ( + 5: the same using stepwise mode)
-PsSYd,  // Save generated SysEq records to data base (+ -)
-PsRes1,    // reserved
-PsRes2,    // reserved
-PsRes3,    // reserved
+   PsSYd,  // Save generated SysEq records to data base (+ -)
+   PsRes1,    // reserved
+   PsRes2,    // reserved
+   PsRes3,    // reserved
 
-name[MAXFORMULA],  //  "Name of DualTh task"
-notes[MAXFORMULA] //  "Comments"
+   name[MAXFORMULA],  //  "Name of DualTh task"
+   notes[MAXFORMULA] //  "Comments"
     ;
-    short
+  short
 // input I
-nQ,   // Q - number of experiments (equilibria) in basis sub-system
-La_b, // Lb - number of formula units to set compositions in basis sub-system
-nK,   // K - number of DC (end-member) candidates in non-basis sub-system
-Nsd,  // N of references to data sources
-Nqpn, // Number of elements in the math script work arrays qpn per non-basis DC
-Nqpg, // Number of elements in the math script work arrays qpg per non-basis DC
-
-Nb,    // N - number of independent components (in basis sub-system, taken
+   nQ,   // Q - number of experiments (equilibria) in basis sub-system
+   La_b, // Lb - number of formula units to set compositions in basis sub-system
+   nK,   // K - number of DC (end-member) candidates in non-basis sub-system
+   Nsd,  // N of references to data sources
+   Nqpn, // Number of elements in the math script work arrays qpn per non-basis DC
+   Nqpg, // Number of elements in the math script work arrays qpg per non-basis DC
+   Nb,    // N - number of independent components (in basis sub-system, taken
         // from project system dimensions automatically)
-Asiz,    //reserved
+   Asiz,    //reserved
 // for generating syseq record keys
-    tmd[3],  // SYSTEM CSD definition #: start, end, step (initial)
-    NVd[3]  // Restrictions variant #: start, end, step
- ; // to discuss !!!!!!!!!!!!!!
-    float
-    Pd[3],    // Pressure P, bar: start, end, increment
-    Td[3],    // Temperature T, C: start, end, increment
-    Vd[3],    // Volume of the system (L): start, end, increment
+   tmd[3],  // SYSTEM CSD definition #: start, end, step (initial)
+   NVd[3]  // Restrictions variant #: start, end, step
+   ;
+  float
+   Pd[3],    // Pressure P, bar: start, end, increment
+   Td[3],    // Temperature T, C: start, end, increment
+   Vd[3],    // Volume of the system (L): start, end, increment
 
   // Input I
-Msysb, // Masses (kg) and volumes (L) for basis sub-system: Ms (total mass, normalize)
-Vsysb, // Vs (total volume of the object, for volume concentrations)
-Mwatb, // M(H2O) (mass of water-solvent for molalities)
-Maqb,  // Maq (mass of aqueous solution for ppm etc.)
-Vaqb,  // Vaq (volume of aqueous solution for molarities)
-Pgb,   // Pg (pressure in gas, for partial pressures)
-Tmolb, // MOL total mole amount for basis sub-system composition calculations
-WmCb,  // molar mass of carrier DC (e.g. sorbent or solvent), reserved
-    cT, cP, cV,  // "State factors T,P,V "
-Flres    //reserved
+   Msysb, // Masses (kg) and volumes (L) for basis sub-system: Ms (total mass, normalize)
+   Vsysb, // Vs (total volume of the object, for volume concentrations)
+   Mwatb, // M(H2O) (mass of water-solvent for molalities)
+   Maqb,  // Maq (mass of aqueous solution for ppm etc.)
+   Vaqb,  // Vaq (volume of aqueous solution for molarities)
+   Pgb,   // Pg (pressure in gas, for partial pressures)
+   Tmolb, // MOL total mole amount for basis sub-system composition calculations
+   WmCb,  // molar mass of carrier DC (e.g. sorbent or solvent), reserved
+   cT,
+   cP,
+   cV,  // State factors T,P,V 
+   Flres    //reserved
     ;
-    double
-*Bb,   //  [Q][N] Table of bulk compositions of basis sub-systems
-*Bn,   //  [Q][N] Table of bulk compositions of non-basis sub-systems
-*Ub,   //  [Q][N] Table of dual solution values for basis sub-systems
-*chi,   //  [Q][K] Table of mole fractions of DC (end-member) candidates
-*mu_n,  //  [Q][K] Table of DualTh chemical potentials of K DC (end-member)
+ double
+   *Bb,   //  [Q][N] Table of bulk compositions of basis sub-systems
+   *Bn,   //  [Q][N] Table of bulk compositions of non-basis sub-systems
+   *Ub,   //  [Q][N] Table of dual solution values for basis sub-systems
+   *chi,   //  [Q][K] Table of mole fractions of DC (end-member) candidates
+   *mu_n,  //  [Q][K] Table of DualTh chemical potentials of K DC (end-member)
            // candidates in Q experiments
-*Coul,  //  [Q][K] Table of Coulombic terms for DC candidates (optional)
-*gam_n, //  [Q][K] Table of activity coefficients for DC candidates
-*avg_g, //  [K] mean over gam_n columns (experiments) for DC candidates
-*sd_g,  //  [K] st.deviation over gam_n columns (experiments) for DC candidates
-*muo_n, //  [Q][K] Table of standard Gibbs energies for DC candidates
-*avg_m, //  [K] mean over muo_n columns (experiments) for DC candidates
-*sd_m,  //  [K] st.deviation over muo_n columns (experiments) for DC candidates
+   *Coul,  //  [Q][K] Table of Coulombic terms for DC candidates (optional)
+   *gam_n, //  [Q][K] Table of activity coefficients for DC candidates
+   *avg_g, //  [K] mean over gam_n columns (experiments) for DC candidates
+   *sd_g,  //  [K] st.deviation over gam_n columns (experiments) for DC candidates
+   *muo_n, //  [Q][K] Table of standard Gibbs energies for DC candidates
+   *avg_m, //  [K] mean over muo_n columns (experiments) for DC candidates
+   *sd_m,  //  [K] st.deviation over muo_n columns (experiments) for DC candidates
+// *muo_i,   // [K] input st.state chem. potentials for candidates (or EMPTY if unknown)
 // *act_n,   // [Q][K] table of DualTh-calculated activities
-*qpn,   //  [Nqpn] Work array for chi calculation math script
-*qpg    //  [Nqpg] Work array for gamma calculation math script
+   *qpn,   //  [Nqpn] Work array for chi calculation math script
+   *qpg    //  [Nqpg] Work array for gamma calculation math script
     ;
-    float
-*CIb, // [Q][N] Table of quantity/concentration of IC in basis sub-systems
-*CIn, // [K][N] Table of quantity/concentration of IC in non-basis sub-systems
-*CAb, // [Q][Lb] Table of quantity/concentration of formulae for basis sub-systems
-*CAn; // [Q][K] Table of quantity/concentration of DC formulae for non-basis sub-systems
-
-char
-*cExpr,  // Math script text for calculation of mole fractions of DC in non-basis
-*gExpr,  // Math script text for calculation of activity coeffs of DC in non-basis
-(*sdref)[V_SD_RKLEN], // "List of bibl. refs to data sources" [0:Nsd-1]
-(*sdval)[V_SD_VALEN],  // "Parameters taken from the respective data sources"[0:Nsd-1]
-(*nam_b)[MAXIDNAME], // [Q][16] id names of experiments
-(*nam_n)[MAXIDNAME], // [K][16] id names of DC (end-member) stoichiometry candidates
-(*for_n)[MAXFORMUNITDT], // [K][40] formulae of DC (end-member) stoichiometry candidates
-(*for_b)[MAXFORMUNITDT], // [Lb][40] formulae for setting basis system compositions
-(*stld)[EQ_RKLEN], // List of SysEq record keys [Q]
-
-*typ_n, // [K] type code of DC stoichiometry candidates {O M J I S ... }
-*CIclb, // [N] Units of IC quantity/concentration for basis compositions
-*CIcln, // [N] Units of IC quantity/concentration for non-basis compositions
-*AUclb, // [Lb] Units of setting quantities of formula units for basis system composition
-*AUcln, // [K] Units of setting quantities of DC formulae for non-basis system composition
-
-    (*SBM)[MAXICNAME+MAXSYMB] // "Keys (names) of IC" [0:Nr-1] ??????????
-
+ float
+   *CIb, // [Q][N] Table of quantity/concentration of IC in basis sub-systems
+   *CIn, // [K][N] Table of quantity/concentration of IC in non-basis sub-systems
+   *CAb, // [Q][Lb] Table of quantity/concentration of formulae for basis sub-systems
+   *CAn  // [Q][K] Table of quantity/concentration of DC formulae for non-basis sub-systems
+//  *Tdq,    //  [Q]  Temperatures of experiment
+//  *Pdq     //  [Q]  Pressures of experiment
     ;
-    /* Work arrays */
-float
- *An;  // [K][N] stoich matrix for DC (end-member) stoichiometry candidates
-char sykey[EQ_RKLEN+10],    // "Key of currently processed SysEq record"
-    *tprn;              // "internal"
-//work
-short
- q,      // index of experiment
- i,      // index of IC
- jm,     // index of non-basis sub-system component
- c_tm,         // Current Tm - SYSTEM CSD number
- c_NV;         // Current Nv - MTPARM variant number
- char timep[16], TCp[16], Pp[16], NVp[16], Bnamep[16];
+ char
+   *cExpr,  // Math script text for calculation of mole fractions of DC in non-basis
+   *gExpr,  // Math script text for calculation of activity coeffs of DC in non-basis
+   (*sdref)[V_SD_RKLEN], // "List of bibl. refs to data sources" [0:Nsd-1]
+   (*sdval)[V_SD_VALEN],  // "Parameters taken from the respective data sources"[0:Nsd-1]
+   (*nam_b)[MAXIDNAME], // [Q][16] id names of experiments
+   (*nam_n)[MAXIDNAME], // [K][16] id names of DC (end-member) stoichiometry candidates
+   (*for_n)[MAXFORMUNITDT], // [K][40] formulae of DC (end-member) stoichiometry candidates
+   (*for_b)[MAXFORMUNITDT], // [Lb][40] formulae for setting basis system compositions
+   (*stld)[EQ_RKLEN], // List of SysEq record keys [Q]
+//
+   *typ_n, // [K] type code of DC stoichiometry candidates {O M J I S ... }
+   *CIclb, // [N] Units of IC quantity/concentration for basis compositions
+   *CIcln, // [N] Units of IC quantity/concentration for non-basis compositions
+   *AUclb, // [Lb] Units of setting quantities of formula units for basis system composition
+   *AUcln, // [K] Units of setting quantities of DC formulae for non-basis system composition
+//
+   (*SBM)[MAXICNAME+MAXSYMB] // Keys (names) of IC
+   ;
+/* Work arrays */
+ float
+   *An;  // [K][N] stoich matrix for DC (end-member) stoichiometry candidates
+   char sykey[EQ_RKLEN+10],    // Key of currently processed SysEq record
+   *tprn;              // internal
+//work data
+ short
+   q,      // index of experiment
+   i,      // index of IC
+   jm,     // index of non-basis sub-system component
+   c_tm,         // Current Tm - SYSTEM CSD number
+   c_NV;         // Current Nv - MTPARM variant number
+   char timep[16], TCp[16], Pp[16], NVp[16], Bnamep[16];
 }
-
 DUALTH;
 
 /* Work objects for DualTh scripts */
