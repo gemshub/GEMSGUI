bb53658297aa6dc0a4d10247b34372dbe51bd27a
gems <gems@7d9feeff-4400-0410-95ed-9cb8b92a518d>
2004-07-16 15:24:11 +0000

    More improvements in makefile for Linux
    git-svn-id: svn+ssh://savannah01.psi.ch/repos/gems/GEMSELEKTOR/trunk@354 7d9feeff-4400-0410-95ed-9cb8b92a518d

--- gems2/rungems.sh
+++ gems2/rungems.sh
@@ -3,7 +3,7 @@
 if [ "`uname`" == "Darwin" ]; then 
 exec `pwd`/gems2.app/Contents/MacOS/gems2 $@
 else
-#exec ./gems2 -c -s ~/Gems2Data/program -u ~/Gems2Data
+#exec ./gems2 -c -d -s ./shared -u ./shared
 exec ./gems2 -c $@
 fi
 
