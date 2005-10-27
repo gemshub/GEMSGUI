d158d4698119028855b3f2ccf375620c3908f5e6
gems <gems@7d9feeff-4400-0410-95ed-9cb8b92a518d>
2005-10-27 07:41:27 +0000

    Preparation of release v.2.1.0 rc0 for Win32. Win95 and NT are no more supported (DK)
    git-svn-id: svn+ssh://savannah01.psi.ch/repos/gems/GEMSELEKTOR/trunk@605 7d9feeff-4400-0410-95ed-9cb8b92a518d

--- gems2/makew_installer.bat
+++ gems2/makew_installer.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\NSIS\makensisw.exe" "C:\Program Files\NSIS\makensis.exe" /CD gems200-w32-install.nsi
+"C:\Program Files\NSIS\makensisw.exe" "C:\Program Files\NSIS\makensis.exe" /CD gems-w32-install.nsi
