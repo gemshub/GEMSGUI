b7324e8c82d96b9ea6be3113db0b0a7476269895
wagner <wagner@7d9feeff-4400-0410-95ed-9cb8b92a518d>
2008-12-16 20:27:42 +0000

    First draft implementation of Soave-Redlich-Kwong (SRK) fluid model (TW)
    git-svn-id: svn+ssh://savannah01.psi.ch/repos/gems/GEMSELEKTOR/trunk@1159 7d9feeff-4400-0410-95ed-9cb8b92a518d

--- gems2/mods/s_fgl2.cpp
+++ gems2/mods/s_fgl2.cpp
@@ -1,7 +1,7 @@
 //-------------------------------------------------------------------
 // $Id$
 //
-// Copyright (c) 2007,2008  Th.Wagner, D.Kulik, S.Dmitrieva
+// Copyright (c) 2007-2008  T.Wagner, D.Kulik, S.Dmitrieva
 //
 // Implementation of the TSolMod class
 // and TVanLaar, TRegular, TRedlichKister, TNRTL and TWilson classes
@@ -78,7 +78,7 @@ TVanLaar::TVanLaar( long int NSpecies, long int NParams, long int NPcoefs, long 
         long int* arIPx, double* arIPc, double* arDCc,
         double *arWx, double *arlnGam, double *aphVOL,
         double dW, double eW ):
-        	TSolMod( NSpecies, NParams, NPcoefs, MaxOrder, NPperDC, 0, 
+        	TSolMod( NSpecies, NParams, NPcoefs, MaxOrder, NPperDC, 0,
         			 T_k, P_bar, Mod_Code, arIPx, arIPc, arDCc, arWx,
         			 arlnGam, aphVOL, dW, eW )
 {
@@ -223,7 +223,7 @@ TRegular::TRegular( long int NSpecies, long int NParams, long int NPcoefs, long 
         long int* arIPx, double* arIPc, double* arDCc,
         double *arWx, double *arlnGam, double *aphVOL,
         double dW, double eW ):
-        	TSolMod( NSpecies, NParams, NPcoefs, MaxOrder, NPperDC, 0, 
+        	TSolMod( NSpecies, NParams, NPcoefs, MaxOrder, NPperDC, 0,
         			 T_k, P_bar, Mod_Code, arIPx, arIPc, arDCc, arWx,
         			 arlnGam, aphVOL, dW, eW )
 {
