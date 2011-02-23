1839802bf954eed39ce3ea62ebed6dc6cbaf01d7
gems <gems@7d9feeff-4400-0410-95ed-9cb8b92a518d>
2011-02-23 10:36:41 +0000

    Updated the kernel NagraPSI 12/07 database in DB.default; will be effective from rev. 1830 (DK)
    git-svn-id: svn+ssh://savannah01.psi.ch/repos/gems/GEMSELEKTOR/trunk@1829 7d9feeff-4400-0410-95ed-9cb8b92a518d

--- gems3/vizor4/dlg4/HelpWindow.cpp
+++ gems3/vizor4/dlg4/HelpWindow.cpp
@@ -230,7 +230,7 @@ trUtf8("GEMS3 (Windows XP/7 MinGW gcc4.4)"),
 #endif
            trUtf8("This is GEM-Selektor 3 (GEMS3) program package   \n\n"
                   "    release candidate version 3.0,\n\n"
-                  "    rev. 1826 (February 22, 2010)\n\n"
+                  "    rev. 1830 (February 24, 2010)\n\n"
                   "    using GEMS3K rev. 524 (trunk) \n\n\n"
                   "For GEMS Wiki community of geochemical modellers \n\n"
                   "(c) 2011, PSI-ETHZ GEMS Development Team\n") );
