2ae69e52a9c64767d3dd361c61180fdcf2ef572e
gems <gems@7d9feeff-4400-0410-95ed-9cb8b92a518d>
2011-06-17 14:59:55 +0000

    Small improvements in uDD detection; added an option to trace moving 3-averages of u_i into ipmlog.txt file (DK). To activate this option, set Pa_LLG != 0 and Pa_PRD[2]=3. Setting Pa_LLG = 30000 allows complete trace of IPM dual solution variations even if it diverges.
    git-svn-id: svn+ssh://savannah01.psi.ch/repos/gems/GEMSELEKTOR/trunk@1861 7d9feeff-4400-0410-95ed-9cb8b92a518d

--- gems3/vizor4/dlg4/HelpWindow.cpp
+++ gems3/vizor4/dlg4/HelpWindow.cpp
@@ -230,8 +230,8 @@ trUtf8("GEMS3 (Windows XP/7 MinGW gcc4.4)"),
 #endif
            trUtf8("This is GEM-Selektor 3 (GEMS3) program package   \n\n"
                   "    release candidate 3.0,\n\n"
-                  "    rev. 1858 (June 16, 2011)\n\n"
-                  "    using GEMS3K rev. 546 (trunk) \n\n\n"
+                  "    rev. 1860 (June 17, 2011)\n\n"
+                  "    using GEMS3K rev. 550 (trunk) \n\n\n"
                   "For GEMS Wiki community of geochemical modellers \n\n"
                   "(c) 2011, PSI-ETHZ GEMS Development Team\n") );
 }
