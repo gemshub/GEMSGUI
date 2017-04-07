ed86af5ad1dd9496df356348912780caad6596c4
kulik <dmitrii.kulik@psi.ch>
2017-04-07 13:06:29 +0200

    Changed revision number to c. ee754fe for the v.3.3.5 bugfix release and fixed the self-extractor settings file accordingly.

--- GUI/Dialogs4/HelpWindow.cpp
+++ GUI/Dialogs4/HelpWindow.cpp
@@ -43,7 +43,7 @@
 const char *GEMS_HOWHELP_HTML = "gems_miscel.html#HOWHELP";
 const char *GEMS_ABOUT_HTML = "gems_about.html#PAGE_ABOUT";
 
-const char *_GEMS_version_stamp = " GEMS-GUI v.3.3.4 c.3cecb08 ";
+const char *_GEMS_version_stamp = " GEMS-GUI v.3.3.5 c.ee754fe ";
 extern const char *_GEMIPM_version_stamp;
 
 HelpWindow* HelpWindow::pDia = 0;
