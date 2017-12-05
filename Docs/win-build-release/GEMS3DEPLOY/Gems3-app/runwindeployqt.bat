00190ddb05cf90691825a558fe093f19f55804db
kulik <dmitrii.kulik@psi.ch>
2017-12-05 17:43:37 +0100

    Tested on Windows 7 and prepared an installer sfx for GEM-Selektor v.3.4.0 (for testing new sorption B&B implementation)

--- Docs/win-build-release/GEMS3DEPLOY/Gems3-app/runwindeployqt.bat
+++ Docs/win-build-release/GEMS3DEPLOY/Gems3-app/runwindeployqt.bat
@@ -1,4 +1,4 @@
 echo off
 echo Setting up environment for Qt usage - see qtenv2.bat in mingw48 bin subfolder
-set PATH=C:\Qt\5.4\mingw491_32\bin;C:\Qt\Tools\mingw491_32\bin;%PATH%
+set PATH=C:\Qt\5.9.2\mingw53_32\bin;C:\Qt\Tools\mingw530_32\bin;%PATH%
 windeployqt %1
