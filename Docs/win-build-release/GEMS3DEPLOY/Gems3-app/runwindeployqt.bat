fe86938eb11b4cef88e91bfe2f564d01c7ca2f39
D.Kulik <dmitrii.kulik@psi.ch>
2018-11-21 09:37:22 +0100

    Corrections in deployment files and scripts for Windows installer; added missing DB.default files.

--- Docs/win-build-release/GEMS3DEPLOY/Gems3-app/runwindeployqt.bat
+++ Docs/win-build-release/GEMS3DEPLOY/Gems3-app/runwindeployqt.bat
@@ -1,4 +1,5 @@
 echo off
-echo Setting up environment for Qt usage - see qtenv2.bat in mingw48 bin subfolder
-set PATH=C:\Qt\5.10.1\mingw53_32\bin;C:\Qt\Tools\mingw530_32\bin;%PATH%
+
+echo Setting up environment for Qt usage - see qtenv2.bat
+PATH=C:\Qt\5.11.2\mingw53_32\bin;C:\Qt\Tools\mingw530_32\bin;%PATH%
 windeployqt %1
