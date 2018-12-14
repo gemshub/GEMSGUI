6632d4ddb0daa189be4013dd0134ac7ca6833935
kulik <dmitrii.kulik@psi.ch>
2018-12-14 11:23:07 +0100

    Bumped versio 3.4.4 and commit number for the bugfix release.

--- GUI/Dialogs4/HelpWindow.cpp
+++ GUI/Dialogs4/HelpWindow.cpp
@@ -43,7 +43,7 @@
 const char *GEMS_HOWHELP_HTML = "gems_miscel.html#HOWHELP";
 const char *GEMS_ABOUT_HTML = "gems_about.html#PAGE_ABOUT";
 
-const char *_GEMS_version_stamp = " GEMS-GUI v.3.4.3 c.597a65a ";
+const char *_GEMS_version_stamp = " GEMS-GUI v.3.4.4 c.0c19d8d ";
 extern const char *_GEMIPM_version_stamp;
 
 HelpWindow* HelpWindow::pDia = 0;
