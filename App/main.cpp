aba6edcdd885c4183503d344e4b9cd45386149b9
svetad <sv.dmytr@gmail.com>
2022-09-15 13:55:48 +0300

    GEMS-207: Added function for setting up logging level and logging to file.

--- App/main.cpp
+++ App/main.cpp
@@ -61,6 +61,7 @@ TIntegApp::TIntegApp(int& c, char** v):
     //cout << "QSqlDatabase: available drivers:" <<
     //      QSqlDatabase::drivers().join(QLatin1String(" ")).toStdString() << endl;
 
+    gems3k_update_loggers( true, "gems3k_gui.log", spdlog::level::info);
     gui_logger->set_level(spdlog::level::debug);
     shMemory.setKey("gems3");
     if( shMemory.attach())
