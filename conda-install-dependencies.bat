3d6485e5bf930e8426ad5efda9f28978889f31a4
svetad <sv.dmytr@gmail.com>
2025-03-12 13:43:35 +0200

    Hide solmod

--- conda-install-dependencies.bat
+++ conda-install-dependencies.bat
@@ -45,7 +45,7 @@ git clone https://github.com/sdmytrievs/GEMS3K.git
 cd GEMS3K
 
 echo "Configuring..."
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_GEM2MT=OFF -DCMAKE_INSTALL_PREFIX:PATH="%CONDA_PREFIX%\Library" -A x64 -S . -B build
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_GEM2MT=OFF  -DBUILD_SOLMOD=OFF -DCMAKE_INSTALL_PREFIX:PATH="%CONDA_PREFIX%\Library" -A x64 -S . -B build
 echo "Building..."
 cmake --build build --target install  --config Release
 
