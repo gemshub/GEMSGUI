e4f7fefc323ef2640648e799d15c6330c6ce51ea
svetad <sv.dmytr@gmail.com>
2025-10-23 20:10:35 +0300

    Run gems3k-export when linux deploy

--- Modules/gemsgui_version.h
+++ Modules/gemsgui_version.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define GEMSGUI_VERSION "3.10.1"
-#define GEMSGUI_VERSION_HASH "4adae190"
+#define GEMSGUI_VERSION_HASH "1d54cb17"
 #define GEMSGUI_GIT_BRANCH "export_projecs_app"
 #define GEMSGUI_OSX "Linux"
 #define GEMSGUI_COMPILER_ID "GNU"
