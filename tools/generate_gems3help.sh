d83567b034cf0c7903a11ffaec53f51b5e9a4a95
svetad <sv.dmytr@gmail.com>
2026-01-26 21:22:03 +0200

    Search qt6 applications

--- tools/generate_gems3help.sh
+++ tools/generate_gems3help.sh
@@ -1,5 +1,9 @@
 cd tools
 ./gems3helpconfig-qhp-generator
-qhelpgenerator gems3helpconfig.qhcp -o Resources/help/gems3help.qhc
+/usr/share/miniconda/envs/GEMSGUI/lib/qt6/qhelpgenerator gems3helpconfig.qhcp -o Resources/help/gems3help.qhc
 cp -r Resources/help ../../Resources
 cd ..
+#/home/runner/conda_pkgs_dir/qt6-main-6.10.1-hb82b983_4/lib/qt6/qhelpgenerator
+#/usr/share/miniconda/envs/GEMSGUI/lib/qt6/qhelpgenerator
+
+
