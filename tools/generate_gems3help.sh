187b75f06deb2a61becadf4f4090831acf75b7f3
svetad <sv.dmytr@gmail.com>
2026-01-27 10:33:25 +0200

    Try use CONDA_PREFIX

--- tools/generate_gems3help.sh
+++ tools/generate_gems3help.sh
@@ -1,6 +1,6 @@
 cd tools
 ./gems3helpconfig-qhp-generator
-/usr/share/miniconda/envs/GEMSGUI/lib/qt6/qhelpgenerator gems3helpconfig.qhcp -o Resources/help/gems3help.qhc
+$CONDA_PREFIX/lib/qt6/qhelpgenerator gems3helpconfig.qhcp -o Resources/help/gems3help.qhc
 cp -r Resources/help ../../Resources
 cd ..
 #/usr/share/miniconda/envs/GEMSGUI/lib/qt6/qhelpgenerator
