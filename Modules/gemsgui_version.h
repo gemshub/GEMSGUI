50a906bb0ee078bdd3500eb2156f756c9542c7a6
svetad <sv.dmytr@gmail.com>
2026-02-11 19:23:30 +0200

    Build without zmq lib

--- Modules/gemsgui_version.h
+++ Modules/gemsgui_version.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #define GEMSGUI_VERSION "3.11.0"
-#define GEMSGUI_VERSION_HASH "afab2553"
+#define GEMSGUI_VERSION_HASH "cda5994d"
 #define GEMSGUI_GIT_BRANCH "LESTO_project"
 #define GEMSGUI_OSX "Linux"
 #define GEMSGUI_COMPILER_ID "GNU"
 #define GEMSGUI_COMPILER_VERSION "13.3.0"
-#define GEMSGUI_QT_VERSION "6.8.2"
+#define GEMSGUI_QT_VERSION "6.10.2"
