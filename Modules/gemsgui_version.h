348bb16dd5084746653b928566ad92a9642ba2ff
svetad <sv.dmytr@gmail.com>
2026-02-25 18:57:01 +0200

    Added a menu flag to expand/collapse the tree view with the phases composition.

--- Modules/gemsgui_version.h
+++ Modules/gemsgui_version.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define GEMSGUI_VERSION "3.11.1"
-#define GEMSGUI_VERSION_HASH "a2450002"
+#define GEMSGUI_VERSION_HASH "774c9e0f"
 #define GEMSGUI_GIT_BRANCH "test_arm"
 #define GEMSGUI_OSX "Linux"
 #define GEMSGUI_COMPILER_ID "GNU"
