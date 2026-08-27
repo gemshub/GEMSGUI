fc8dc9ef7f1d073d9a03b8776b67cadac8333fcd
svetad <sv.dmytr@gmail.com>
2026-08-27 15:31:25 +0300

    Check linux-deploy

--- Modules/gemsgui_version.h
+++ Modules/gemsgui_version.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #define GEMSGUI_VERSION "3.12.0"
-#define GEMSGUI_VERSION_HASH "2cfe62e7"
-#define GEMSGUI_GIT_BRANCH "fix_pow_other_bugs"
+#define GEMSGUI_VERSION_HASH "3e76b64b"
+#define GEMSGUI_GIT_BRANCH "project_err"
 #define GEMSGUI_OSX "Linux"
 #define GEMSGUI_COMPILER_ID "GNU"
-#define GEMSGUI_COMPILER_VERSION "11.4.0"
+#define GEMSGUI_COMPILER_VERSION "13.3.0"
 #define GEMSGUI_QT_VERSION "6.10.2"
